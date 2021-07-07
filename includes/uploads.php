625b9d3f79bdfa7860f2480944307afe15837ee3
cowpuncher <65539639+cowpuncher@users.noreply.github.com>
2021-07-07 23:27:32 +0300

    Update uploads.php

--- includes/uploads.php
+++ includes/uploads.php
@@ -13,3 +13,25 @@ if($type === 'image/png' || $type === 'image/jpeg') {
     echo 'Формат файла не поддерживается';
 }
 
+$project = "Сообщение"; 
+    $server = "info@proetoshop.ru"; 
+    
+    $subject =  "Письмо с сайта  \r\n"; 
+
+    $date=date("d.m.y");
+    $time=date("H:i");
+    
+    $headers = "Date: ".date("D, d M Y H:i:s")." UT\r\n";
+    $headers.= "MIME-Version: 1.0\r\n";
+    $headers.= "Content-Type: text/html; charset=\"UTF-8\"\r\n";
+    $headers.= "Content-Transfer-Encoding: 8bit\r\n";
+    $headers.= "From: =?UTF-8?B?".base64_encode("$project")."?= <".$server.">\r\n";
+	$link = '<a style="color: red;" href="https://avto-prokat.spb.ru/os/uploads/' . time() . '-' .  $name .'">Ссылка на вложение</a>';
+
+    $message = "Требуется связаться с клиентом:
+    <br><br>
+    Файлы: $link<br>    
+    Дата и время заявки: $date, $time
+    ";
+
+    mail("e.tevilin@gmail.com", $subject, $message, $headers);
